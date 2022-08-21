999ca6c4b1d9be1cc0cb9cb80138d05df15c484a
Georgi Danchev <georgicodes@outlook.com>
2022-08-21 16:13:20 +0100

    updated packge versions

--- src/js/sections/Home.js
+++ src/js/sections/Home.js
@@ -1,15 +1,27 @@
 // import { useRef } from "react"
 import DecorativeBorder from "../components/DecorativeBorder"
 import DownArrow from "../components/DownArrow"
-import particleConfig from "../../assets/particlesjs-config.json"
-import Particles from "react-particles-js"
+// import particleConfig from "../../assets/particlesjs-config.json"
+// import Particles from "react-tsparticles"
+// import { loadFull } from "tsparticles"
+// import { useCallback } from "react"
 import ResponsiveImage from "../components/ResponsiveImage"
 // import useIntersection from "../helpers/useIntersection"
 
 const Home = () => {
-  const { REACT_APP_FIRST_NAME, REACT_APP_LAST_NAME, REACT_APP_JOB_TITLE, REACT_APP_SITE_SUBTITLE } = process.env
+  const {
+    REACT_APP_FIRST_NAME,
+    REACT_APP_LAST_NAME,
+    REACT_APP_JOB_TITLE,
+    REACT_APP_SITE_SUBTITLE,
+  } = process.env
+
   // const home_ref = useRef()
   // useIntersection(home_ref, "home")
+
+  // const particlesInit = useCallback(async (engine) => {
+  //   await loadFull(engine)
+  // }, [])
 
   return (
     <section id="home" className="section section--hero" data-scroll-section>
@@ -25,16 +37,23 @@ const Home = () => {
             {REACT_APP_FIRST_NAME} {REACT_APP_LAST_NAME}
           </span>
           <span className="home-section__title-spacer">|</span>
-          <span className="home-section__title-inner home-section__title-inner--second">{REACT_APP_JOB_TITLE}</span>
+          <span className="home-section__title-inner home-section__title-inner--second">
+            {REACT_APP_JOB_TITLE}
+          </span>
         </h1>
         <p className="home-section__text">{REACT_APP_SITE_SUBTITLE}</p>
       </div>
 
       <div className="section__bg-image-wrapper home">
-        <ResponsiveImage image_class="section__bg-image" code="v1629540939" public_id="hero_rzr91t" alt="Laptop sitting on a desk" />
+        <ResponsiveImage
+          image_class="section__bg-image"
+          code="v1629540939"
+          public_id="hero_rzr91t"
+          alt="Laptop sitting on a desk"
+        />
       </div>
 
-      <Particles params={particleConfig} />
+      {/* <Particles id="tsparticles" options={particleConfig} init={particlesInit} /> */}
 
       <DownArrow />
 
